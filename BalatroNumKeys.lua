3fb1816777216b0bf46856118427bbca36e028ad
Mark Rizko <mark@rizkocircle.com>
2024-03-24 12:59:30 -0700

    Added hotkeys for selecting/play/discard

---
+++ BalatroNumKeys.lua
@@ -0,0 +1,89 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Balatro Num Keys
+--- MOD_ID: BalatroNumKeys
+--- MOD_AUTHOR: [markrizko]
+--- MOD_DESCRIPTION: V1.0.0 - Allows user to use number keys to select cards in hand
+
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+local keyupdate_ref = Controller.key_press_update
+
+function Controller.key_press_update(self, key, dt)
+    keyupdate_ref(self, key, dt)
+    keys_to_nums = {
+        ["1"] = 1,
+        ["2"] = 2,
+        ["3"] = 3,
+        ["4"] = 4,
+        ["5"] = 5,
+        ["6"] = 6,
+        ["7"] = 7,
+        ["8"] = 8,
+        ["9"] = 9,
+        ["0"] = 10,
+        ["-"] = 11,
+        ["="] = 12,
+    }
+    keys_to_ui = {
+        ["`"] = "clear_hand",
+        ["\\"] = "discard hand",
+        ["return"] = "play hand",
+    }
+
+    if G.STATE == G.STATES.SELECTING_HAND then
+        if tableContains(keys_to_nums, key) then
+            num = keys_to_nums[key]
+            in_list = false
+            if num <= #G.hand.cards then
+                card = G.hand.cards[num]
+                for i = #G.hand.highlighted, 1, -1 do
+                    if G.hand.highlighted[i] == card then
+                        in_list = true
+                        break
+                    end
+                end
+                if in_list then
+                    G.hand:remove_from_highlighted(card, false)
+                    play_sound('cardSlide2', nil, 0.3)
+                else
+                    G.hand:add_to_highlighted(card)
+                end
+            end
+        end
+        if tableContains(keys_to_ui, key) then
+            ui = keys_to_ui[key]
+            if ui == "clear_hand" then
+                queue_clear_key_pressed()
+            elseif ui == "discard hand" then
+                local discard_button = G.buttons:get_UIE_by_ID('discard_button')
+                if discard_button.config.button == 'discard_cards_from_highlighted' then
+                    G.FUNCS.discard_cards_from_highlighted()
+                end
+            elseif ui == "play hand" then
+                local play_button = G.buttons:get_UIE_by_ID('play_button')
+                if play_button.config.button == 'play_cards_from_highlighted' then
+                    G.FUNCS.play_cards_from_highlighted()
+                end
+            end
+        end
+    end
+end
+
+function tableContains(table, key)
+    for k, v in pairs(table) do
+        if k == key then
+            return true
+        end
+    end
+    return false
+end
+
+function queue_clear_key_pressed(x, y)
+    if not G.SETTINGS.paused and G.hand and G.hand.highlighted[1] then 
+        if (G.play and #G.play.cards > 0) or
+        (G.GAME.STOP_USE and G.GAME.STOP_USE > 0) then return end
+        G.hand:unhighlight_all()
+    end
+end
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # balatro-numkeys
-Mod to add selecting cards with number keys in Balatro
+Mod to add selecting cards with number keys in Balatro. Also adds hotkeys for playing and discarding hands.
+<!--- 
+Used Milck's QOL as a reference. 
+-->
+
+[Milck's QOL](https://github.com/Milck-QOL) and [Better-Mouse-And-Gamepad](https://github.com/Kooluve/Better-Mouse-And-Gamepad) helped inspire/reference for this code.
+
+
+## How to Use
+| Operations | Functions |
+|----------|----------|
+|   Keys 1-0 and "-", "=" keys  |  Select first 12 cards in hand  |
+|   "\" key  |   Discard hand  |
+|   Return (enter) key  |   Play hand  |
+
+## Installation
+Follow the installation instructions for the Balatro [Steamodded Modloader](https://github.com/Steamopollys/Steamodded) to install
+
+## Contributing
+Pull requests welcome!
+
+## License
+This project is licensed under the GNU General Public License. This ensures that the software is free to use, modify, and distribute. For more details, see the LICENSE file in the repository.
